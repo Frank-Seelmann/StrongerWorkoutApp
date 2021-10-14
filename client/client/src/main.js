4b9721cb1164bda9e8fe14e6ce435711073c559d
frankdaseel <frankseelmann123@gmail.com>
2021-10-14 11:56:49 -0400

    some vue setup

--- client/client/src/main.js
+++ client/client/src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+import '@fortawesome/fontawesome-free';
+import 'bulma';
 
 createApp(App).use(router).mount('#app')
