e839bec68025e3288bd9df28720fa132e41f1f67
frankdaseel <frankseelmann123@gmail.com>
2021-10-15 12:53:12 -0400

    re-initialized vue because something went wrong

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,5 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-
-import '@fortawesome/fontawesome-free';
-import 'bulma';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
 createApp(App).use(router).mount('#app')
