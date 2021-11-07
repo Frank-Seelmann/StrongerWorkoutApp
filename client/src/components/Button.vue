85e54726ecb06393ff1855d20436f9803534b386
frankdaseel <frankseelmann123@gmail.com>
2021-11-07 13:26:12 -0500

    started adding content to tracker

---
+++ client/src/components/Button.vue
@@ -0,0 +1,37 @@
+<template>
+    <div v-if="type === 'success'">
+    <button class="button is-success" @click="onClick()">
+        {{ text }}
+    </button>
+    </div>
+    <div v-else-if="type === 'link'">
+    <button class="button is-link" @click="onClick()">
+        {{ text }}
+    </button>
+    </div>
+    <div v-else>
+    <button class="button is-danger" @click="onClick()">
+        {{ text }}
+    </button>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    type: String,
+  },
+  methods: {
+    onClick() {
+      this.$emit('btn-click')
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
--- client/src/views/Extracker.vue
+++ client/src/views/Extracker.vue
@@ -11,7 +11,10 @@
         <div class="control" id="workout">
           <label class="label">Enter Workout:</label>
           <input class="input" type="text" placeholder="Workout" />
+          <Button text="Add Task" type="link"/>
         </div>
+        <br />
+        <Tasks :tasks="tasks"/>
         <br />
         <div class="container">
           Check out your workout <a href="summary.html">summary and reports</a>!
@@ -43,12 +46,44 @@
 
 <script>
 import foot from "../components/Footer.vue";
+import Button from "../components/Button.vue";
+import Tasks from "../components/Tasks.vue";
 
 export default {
   name: "Extracker",
   components: {
     foot,
+    Button,
+    Tasks
   },
+  //temporary
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Ab Workout',
+        day: 'November 7th at 11:00am',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Arm Workout',
+        day: 'November 8th at 12:00pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Chest Workout',
+        day: 'November 9th at 1:00pm',
+        reminder: true,
+      }
+    ]
+  }
 };
 </script>
 
